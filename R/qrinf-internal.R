5fe263b5e6ed4a64a923a4994e6b9da6b05f9006
Nathan Laroy <laroynathan@gmail.com>
2026-07-09 09:37:38 +0200

    internalize statFun

--- R/qrinf-internal.R
+++ R/qrinf-internal.R
@@ -38,3 +38,8 @@ function(x, form, data){
   )
   as.list(context)
 }
+.statFun <- function(x){
+  B <- x$coefficients[,1]
+  V <- x$cov
+  return(t(R %*% B - r) %*% solve(R %*% V %*% t(R)) %*% (R %*% B - r))
+}
